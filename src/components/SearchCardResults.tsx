36eeab9f5990988a77d2a1aab45179be7e6ff72b
YelYan <yelyan.researchandproduction@gmail.com>
2025-10-29 02:48:31 +0630

    [modified] search card ui

--- src/components/SearchCardResults.tsx
+++ src/components/SearchCardResults.tsx
@@ -8,20 +8,19 @@ const SearchCardResults = ({ restaurant }: { restaurant: Restaurant }) => {
   return (
     <Link to={"/"}>
       <Card>
-        <CardContent className="flex flex-col md:flex-row items-center gap-4">
-          <AspectRatio ratio={25 / 8}>
+        <CardContent className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-center">
+          <AspectRatio ratio={19 / 8}>
             <img
               src={restaurant.imageUrl}
               className="rounded-md w-full h-full object-cover"
             />
           </AspectRatio>
 
-          <div>
-            <h3 className="text-2xl font-bold tracking-tight mb-2 group-hover:underline">
-              {restaurant.restaurantName}
-            </h3>
-
-            <div id="card-content" className="grid md:grid-cols-2 gap-2">
+          <div className="col-span-2 flex flex-col md:flex-row items-center gap-8">
+            <div id="card-content" className="flex flex-col">
+              <h3 className="text-2xl font-bold tracking-tight mb-2 group-hover:underline">
+                {restaurant.restaurantName}
+              </h3>
               <div className="flex flex-row flex-wrap">
                 {restaurant.cuisines.map((item, index) => (
                   <span key={index} className="flex">
@@ -30,16 +29,15 @@ const SearchCardResults = ({ restaurant }: { restaurant: Restaurant }) => {
                   </span>
                 ))}
               </div>
-
-              <div className="flex gap-2 flex-col">
-                <div className="flex items-center gap-1 text-green-600">
-                  <Clock className="text-green-600" />
-                  {restaurant.estimatedDeliveryTime} mins
-                </div>
-                <div className="flex  gap-1">
-                  <Banknote />
-                  Delivery from ${(restaurant.deliveryPrice / 100).toFixed(2)}
-                </div>
+            </div>
+            <div className="flex gap-2 flex-col">
+              <div className="flex items-center gap-1 text-green-600">
+                <Clock className="text-green-600" />
+                {restaurant.estimatedDeliveryTime} mins
+              </div>
+              <div className="flex  gap-1">
+                <Banknote />
+                Delivery from ${(restaurant.deliveryPrice / 100).toFixed(2)}
               </div>
             </div>
           </div>
